3d6af0152721be3cb54c703a2a92766bec8612dd
sssc2023 <143796470+sssc2023@users.noreply.github.com>
2023-09-02 20:19:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,20 +42,11 @@ if uploaded_file is not None:
         is_separator_regex = False,
     )
     texts = text_splitter.split_documents(pages)
-
+    persist_directory = './'
     #Embedding
     embeddings_model = OpenAIEmbeddings()
 
     # load it into Chroma
-    db = Chroma.from_documents(texts, embeddings_model)
-
-    #Question
-    st.header("PDF에게 질문해보세요!!")
-    question = st.text_input('질문을 입력하세요')
-
-    if st.button('질문하기'):
-        with st.spinner('Wait for it...'):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
-            result = qa_chain({"query": question})
-            st.write(result["result"])
+    db = Chroma.from_documents(documents=texts, embedding=embeddings_model, persist_directory=persist_directory)
+    db.persist()
+    db = None
